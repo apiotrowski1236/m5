b2b03cfbddd2e665a98a04090eaed3363eda3abf
annie <anniekatherine.piotrowski@gmail.com>
2020-06-28 18:47:42 +0000

    Second commit.

--- scripts/db_setup_script.sh
+++ scripts/db_setup_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DB=$1
-
+export PGPASSWORD='haroldthecat'
 psql -h $1 -U postgres
-haroldthecat
-CREATE DATABASE image_gallery;
-CREATE TABLE users (username varchar(255) NOT NULL, password varchar(255) NOT NULL, fullname(255) NOT NULL);
-insert into users values('fred', 'foo', 'fred flintstone');
-insert into users values('wima', 'foo', 'Wilma flintstone');
-CREATE USER image_gallery login password 'simple';
-quit
+
+psql -c CREATE DATABASE image_gallery;
+psql -c CREATE TABLE users (username varchar(255) NOT NULL, password varchar(255) NOT NULL, fullname(255) NOT NULL);
+psql -c insert into users values('fred', 'foo', 'fred flintstone');
+psql -c insert into users values('wima', 'foo', 'Wilma flintstone');
+psql -c CREATE USER image_gallery login password 'simple';
+
 
