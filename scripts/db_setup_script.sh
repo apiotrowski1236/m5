cba3e388d1f37e932d3185e06511b234541ba63e
annie <anniekatherine.piotrowski@gmail.com>
2020-06-29 03:09:58 +0000

    Final Sunday commit-working asg

--- scripts/db_setup_script.sh
+++ scripts/db_setup_script.sh
@@ -4,10 +4,9 @@ DB=$1
 export PGPASSWORD='haroldthecat'
 psql -h $1 -U postgres
 
-psql -c CREATE DATABASE image_gallery;
-psql -c CREATE TABLE users (username varchar(255) NOT NULL, password varchar(255) NOT NULL, fullname(255) NOT NULL);
-psql -c insert into users values('fred', 'foo', 'fred flintstone');
-psql -c insert into users values('wima', 'foo', 'Wilma flintstone');
-psql -c CREATE USER image_gallery login password 'simple';
+psql -c 'CREATE TABLE users (username varchar(255) NOT NULL, password varchar(255) NOT NULL, fullname(255) NOT NULL);'
+psql -c 'insert into users values('fred', 'foo', 'fred flintstone');'
+psql -c 'insert into users values('wima', 'foo', 'Wilma flintstone');'
+psql -c 'CREATE USER image_gallery login password 'simple';'
 
 
